8fa034f741db881e29c5798dada29b21bf0d6b03
nsyawali12 <nsyawali12@gmail.com>
2019-05-19 22:33:02 +0700

    tampilan started udh

--- banany_v02/app/src/main/java/com/example/nsyawalia/banany_v02/MainActivity.java
+++ banany_v02/app/src/main/java/com/example/nsyawalia/banany_v02/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, ExtActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this, StartedActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
